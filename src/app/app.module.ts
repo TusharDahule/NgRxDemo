4ab2a1de87b62be20abcec3d4e19aa3ecc41f012
TusharDahule <tushardahule@gmail.com>
2025-11-29 09:01:37 +0530

    login form setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
-import { appReducer } from './store/app.state';
 
 @NgModule({
   declarations: [
